c7dee2e35e3ef8880cc6ff3b79aff87ce35080e2
Box_Designer_group <cgougenheim.third@gmail.com>
2020-05-22 11:22:28 -0500

    initial commit

---
+++ Box_Designer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Box_Designer
+//
+//  Copyright © 2020 Field_Session. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
